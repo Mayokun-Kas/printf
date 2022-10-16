e3fa92b6ca0ebee0477be24ae4bbe8b01e9aca0f
uba2000 <ubanoel2000@gmail.com>
2022-10-16 18:58:00 +0100

    chore: Init a _printf that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib>
+#include <stdio.h>
+
+/**
+ * _printf - similar to prinff in stdio
+ * @format: character string composed of zero or more directives
+ * Return: the number of characters printed
+ * excluding the null byte used to end output to strings
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+	int printedChar = 0;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		_putchar(format[i]);
+		printedChar++;
+	}
+
+	return (printedChar);
+}
