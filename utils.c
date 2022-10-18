049f81a8c1d66e5ce2a2d1ef70ee1493736610a1
uba2000 <ubanoel2000@gmail.com>
2022-10-18 19:46:30 +0100

    team task

--- utils.c
+++ utils.c
@@ -51,7 +51,7 @@ int is_digit(char c)
 	return (0);
 }
 
-	/**
+/**
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
